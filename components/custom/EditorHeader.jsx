718eaea2c44776fc905dcfab2f05c81a52bb9da3
usamaansari <usamazafaransari@gmail.com>
2025-05-07 22:09:38 -0700

    Designed editor heading

--- components/custom/EditorHeader.jsx
+++ components/custom/EditorHeader.jsx
@@ -11,18 +11,15 @@ const EditorHeader = ({viewHTMLCode}) => {
     const router = useRouter();
 
   return (
-    <div className='px-10 md:px-20 lg:px-25 flex flex-row items-center justify-between mt-5 shadow-md'>
+    <div className='px-10 flex flex-row items-center justify-between mt-5 shadow-md'>
         <Image src={'/LemonadeLogo-NoBG.png'} alt='logo' width={165} height={150} onClick={() => router.push("/")} />
         <div>
-            <Button variant="ghost" className={`hover:text-primary hover:bg-purple-200 border-1 ${screenSize == 'desktop' ? 'text-primary bg-purple-200' : ''}`} onClick={() => setScreenSize('desktop')}>
+          <h1 className='text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-yellow-400'>Kids Website Builder</h1>
+        </div>
+        <div className='flex flex-row gap-3'>
+        <Button variant="ghost" className={`hover:text-primary hover:bg-purple-200 border-1 ${screenSize == 'desktop' ? 'text-primary bg-purple-200' : ''}`} onClick={() => setScreenSize('desktop')}>
                 <Monitor /> Desktop</Button>
             <Button variant='ghost' className={`ml-2 hover:text-primary hover:bg-purple-200 border-1 ${screenSize == 'mobile' ? 'text-primary bg-purple-200' : ''}`} onClick={() => setScreenSize('mobile')}><Smartphone /> Mobile</Button>
-
-        </div>
-        <div className='flex gap-3'>
-            <Button variant='ghost' className='hover:text-primary hover:bg-purple-200 border-1' onClick={()=>viewHTMLCode(true)}><Code /></Button>
-            <Button variant="outline"  onClick={()=>viewHTMLCode(true)}>Preview</Button>
-            <Button>Save</Button>
         </div>
     </div>
   )
